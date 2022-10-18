435084629ca905880fa3790141240355b0816d4e
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-10-18 20:30:33 +0500

    refact: extract categories array

--- src/components/category/category.component.jsx
+++ src/components/category/category.component.jsx
@@ -1,35 +1,7 @@
 import CategoryItem from "../category-item/category-item.component";
 import "./category.styles.scss";
 
-const Category = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
+const Category = ({categories}) => {
   return (
     <div className="categories-container">
       {categories.map((category) => (
