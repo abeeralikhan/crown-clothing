f0d23e703f40c970f498169a63462f26eb63c701
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-10-18 20:19:28 +0500

    feat: extract category component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,7 @@
-import CategoryItem from "./components/category-item/category-item.component";
-import "./categories.styles.scss";
+import Category from "./components/category/categories.component";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
-  );
+  return <Category />;
 };
 
 export default App;
