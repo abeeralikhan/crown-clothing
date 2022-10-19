0dab630d59a93d278d81ffdcfd1b7f1fb328bec8
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-10-19 21:25:16 +0500

    feat: make navbar component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./routes/home/home.component";
 
-const Cart = () => {
-  return <h1>Welcome to the Cart!</h1>;
+import Home from "./routes/home/home.component";
+import Navbar from "./routes/navbar/navbar.component";
+
+const Shop = () => {
+  return <h1>Welcome to the Shop!</h1>;
 };
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />}>
-        <Route path="cart" element={<Cart />} />
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
   );
