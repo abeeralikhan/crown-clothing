46d27dc4fbfa5dc3e434bf2e828f59960477424f
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-10-20 19:30:21 +0500

    feat: create a sign in page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 import Navbar from "./routes/navbar/navbar.component";
+import SignIn from "./routes/sign-in/sign-in.component";
 
 const Shop = () => {
   return <h1>Welcome to the Shop!</h1>;
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="/" element={<Navbar />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
